dd746e86f4a07614dcb5bef2729fb7e75eeed71f
malavmevada <malavmevada2000@gmail.com>
2021-04-29 22:04:27 +0530

    created user model.

--- LinkedIn_Clone/settings.py
+++ LinkedIn_Clone/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     # our apps
     'UserApp',
     'PostApp',
@@ -72,14 +74,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'LinkedIn_Clone.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'linkedindb',
+        'USER': 'postgres',
+        'PASSWORD': '556232119',
+        'HOST': '127.0.0.1',
     }
 }
 
--- UserApp/views.py
+++ UserApp/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 
-# Create your views here.
+
+# First get the all the users and then display in a way mentioned in UserSerializers.
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+'''
+Token Authentication needed so we use custome auth_token instead of ObatainAuthToken
+because it gives more flexibility.
+'''
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        if User.objects.filter(username=request.data['username']).exists():
+            serializer = self.serializer_class(data=request.data,
+                                               context={'request': request})
+            if serializer.is_valid():
+                user = serializer.validated_data['user']
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({
+                    'token': token.key,
+                    'first_name':user.first_name,
+                    # 'user_id': user.pk,
+                    # 'email': user.email
+                })
+            return Response({"error": "Please check your Username or Password", "msg": "1"},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({"error": "User does not exixts", "msg": "2"}, status=status.HTTP_404_NOT_FOUND)
