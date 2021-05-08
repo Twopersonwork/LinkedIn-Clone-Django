504f73f8c784c108a12084e23ce152c89fe8c7bb
malavmevada <malavmevada2000@gmail.com>
2021-05-08 15:29:31 +0530

    added license serializer in User Serializer

--- ProfileApp/models.py
+++ ProfileApp/models.py
@@ -35,8 +35,8 @@ class License(models.Model):
     user = models.ForeignKey(User, related_name="user_license", on_delete=models.CASCADE)
     name = models.CharField(max_length=200, blank=True, null=True)
     issuing_org = models.CharField(max_length=200, blank=True, null=True)
-    issue_date = models.DateField(blank=True, null=True)
-    expiration_date = models.DateField(blank=True, null=True)
+    issue_date = models.DateField(default=None,blank=True, null=True)
+    expiration_date = models.DateField(default=None,blank=True, null=True)
     credential_id = models.CharField(max_length=100, blank=True, null=True)
 
 
