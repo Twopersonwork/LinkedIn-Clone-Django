ca3c0657d3315dfd1c2e6cc2ab63dfc396a13ae4
prachijpatel <prachi.jpatel1@gmail.com>
2021-05-09 11:48:53 +0530

    removed pycache folders from all apss and git as well

--- PostApp/views.py
+++ PostApp/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-import datetime
+
 
 
 # to create the post
