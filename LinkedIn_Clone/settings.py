64ecb9d6a6c26b685b9a3136e921a3b99c515424
prachijpatel <prachi.jpatel1@gmail.com>
2021-05-19 12:40:27 +0530

    added django-on-heroku in settings and requirements

--- LinkedIn_Clone/settings.py
+++ LinkedIn_Clone/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -178,3 +179,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# activate heroku settings for djnago
+django_on_heroku.settings(locals())
