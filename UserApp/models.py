b5197403dcc45f998efc0ce31f0e5116a3acabe3
prachijpatel <prachi.jpatel1@gmail.com>
2021-05-24 22:06:42 +0530

    deleted pre-delete method that was not even working

--- UserApp/models.py
+++ UserApp/models.py
@@ -67,9 +67,7 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
-@receiver(pre_delete, sender=settings.AUTH_USER_MODEL)
-def photo_delete(sender, instance, **kwargs):
-    cloudinary.uploader.destroy(instance.image.public_id)
+
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
