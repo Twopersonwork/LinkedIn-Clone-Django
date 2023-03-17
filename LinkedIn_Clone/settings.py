52218710c5248cbe0c279d35f1fe2c2d442dcb20
Prachi Patel <prachi.jpatel1@gmail.com>
2023-03-18 04:38:55 +0530

    added urls to eliminate cors origin errors

--- LinkedIn_Clone/settings.py
+++ LinkedIn_Clone/settings.py
@@ -39,7 +39,9 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3001',
     'http://localhost:19006',
     'https://ditto-linkedin.herokuapp.com',
-    'http://ditto-linkedin.herokuapp.com'
+    'http://ditto-linkedin.herokuapp.com',
+    'https://ditto-linkedin.vercel.app',
+    'http://ditto-linkedin.vercel.app'
 ]
 
 
